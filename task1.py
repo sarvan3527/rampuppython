297d7f2e5c55f26b1d59768dd50c2fc2f9dbb542
sarvan3527 <141622240+sarvan3527@users.noreply.github.com>
2023-09-20 14:17:04 +0530

    Update task1.py

--- task1.py
+++ task1.py
@@ -6,27 +6,34 @@ def calculate_rectangle_area(length, width):
 def calculate_circle_area(radius):
     return math.pi * radius ** 2
 
-def calculate_triangle_area(base, height):
-    return 0.5 * base * height
+def calculate_triangle_area(base, height, shape):
+    if shape == "triangle":
+        return 0.5 * base * height
 
 try:
     # Get input from the user
-    shape = input("Enter the shape (rectangle, circle, or triangle): ").lower()
+    num_args = int(input("Enter the number of arguments (1, 2, or 3): "))
 
-    if shape == "rectangle":
+    if num_args == 1:
+        radius = float(input("Enter the radius: "))
+        print("Circle Area:", calculate_circle_area(radius))
+
+    elif num_args == 2:
         length = float(input("Enter the length: "))
         width = float(input("Enter the width: "))
         print("Rectangle Area:", calculate_rectangle_area(length, width))
-    elif shape == "circle":
-        radius = float(input("Enter the radius: "))
-        print("Circle Area:", calculate_circle_area(radius))
-    elif shape == "triangle":
+
+    elif num_args == 3:
         base = float(input("Enter the base: "))
         height = float(input("Enter the height: "))
-        print("Triangle Area:", calculate_triangle_area(base, height))
+        shape = input("Enter the shape type (triangle): ").lower()
+        if shape == "triangle":
+            print("Triangle Area:", calculate_triangle_area(base, height, shape))
+        else:
+            print("Invalid shape entered. Please enter 'triangle'.")
+
     else:
-        print("Invalid shape entered. Please enter 'rectangle', 'circle', or 'triangle'.")
-except ValueError:
-    print("Invalid input. Please enter valid numeric values for the dimensions.")
-except Exception as e:
-    print("An error occurred:", str(e))
+        print("Invalid number of arguments. Please enter 1, 2, or 3.")
+
+except:
+    print("Invalid input. Please enter valid numeric values.")
