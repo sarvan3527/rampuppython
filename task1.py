d86c4813c95496608a0bd6fc95d29ae77ed59952
sarvan3527 <141622240+sarvan3527@users.noreply.github.com>
2023-09-21 12:46:03 +0530

    Update task1.py

--- task1.py
+++ task1.py
@@ -11,26 +11,33 @@ def calculate_triangle_area(base, height, shape):
         return 0.5 * base * height
 
 try:
-    # Get input from the user
     num_args = int(input("Enter the number of arguments (1, 2, or 3): "))
 
     if num_args == 1:
         radius = float(input("Enter the radius: "))
-        print("Circle Area:", calculate_circle_area(radius))
+        if radius > 0:
+            print("Circle Area:", calculate_circle_area(radius))
+        else:
+            print("Radius must be a positive value.")
 
     elif num_args == 2:
         length = float(input("Enter the length: "))
         width = float(input("Enter the width: "))
-        print("Rectangle Area:", calculate_rectangle_area(length, width))
+        if length > 0 and width > 0:
+            print("Rectangle Area:", calculate_rectangle_area(length, width))
+        else:
+            print("Length and width must be positive values.")
 
     elif num_args == 3:
         base = float(input("Enter the base: "))
         height = float(input("Enter the height: "))
         shape = input("Enter the shape type (triangle): ").lower()
-        if shape == "triangle":
+        if shape == "triangle" and base > 0 and height > 0:
             print("Triangle Area:", calculate_triangle_area(base, height, shape))
-        else:
+        elif shape != "triangle":
             print("Invalid shape entered. Please enter 'triangle'.")
+        else:
+            print("Base and height must be positive values for a triangle.")
 
     else:
         print("Invalid number of arguments. Please enter 1, 2, or 3.")
